d5427fd820015423329263eac40271f708945499
Toby Cox <eagerterrier@gmail.com>
2017-08-04 13:54:04 +0100

    INI: first commit
    Signed-off-by: Toby Cox <eagerterrier@gmail.com>

---
+++ InstagramfeedComponent.php
@@ -0,0 +1,96 @@
+<?php
+
+App::uses('Component', 'Controller');
+
+/**
+ * Instagramfeed 
+ *
+ * @package App.Controller.Component
+ */
+class InstagramfeedComponent extends Component
+{
+    public $endpoint = 'https://www.instagram.com/%s/media/';
+    public $format = 'json';
+    public $errors = array();
+
+    /**
+     * Creates url representing endpoint based on specific params
+     *
+     * @param string $uri Url which should be queried
+     * @param string $format Format to use for response (json or xml)
+     *
+     * @author Pawel Drylo <pawel.drylo@netro42.com>
+     * @return string
+     */
+    protected function getEndpoint($username, $format = 'json')
+    {
+        return sprintf($this->endpoint, $username);
+    }
+
+    /**
+     * Fetches feed from instagram and returns as many items as you request with $limit
+     *
+     * @param string $username Instagram username 
+     *
+     * @author Toby Cox <toby.cox@netro42.com>
+     * @return array
+     */
+    public function getFeedFromInstagram(string $endpoint, int $limit)
+    {
+        $returned = $this->request($endpoint);
+        if (!$returned) {
+            $this->errors[] = 'Instagram did not return any results';
+            return false;
+        }
+
+        // TODO - put following line into the conditional clause if possible.
+        $_returned = json_decode($returned, true);
+        if (count($_returned['items']) > $limit) {
+            $status = $_returned['status'];
+            $more_available = $_returned['more_available'];
+            $_returned = array_chunk($_returned['items'], $limit);
+            $returned = json_encode(array('items' => $_returned[0], 'more_available' => $more_available, 'status' => $status));
+        }
+        unset($_returned);
+
+        return $returned;
+    }
+
+    /**
+     * Fetches feed from instagram
+     *
+     * @param string $username Instagram username 
+     *
+     * @author Toby Cox <toby.cox@netro42.com>
+     * @return array
+     */
+    public function getFeed(string $username, int $limit)
+    {
+        if (!is_integer($limit)) {
+            $this->errors[] = 'Limit must be a number';
+        } elseif ($limit > 20) {
+            $this->errors[] = 'Instagram have a limit of 20 for the maximum';
+        }
+        return (count($this->errors) ? json_encode(array('errors' => $this->errors)) : $this->getFeedFromInstagram($this->getEndpoint($username, $this->format), $limit));
+    }
+
+    /**
+     * Request Instagram feed
+     *
+     * @param string $endpoint Endpoint
+     *
+     * @author Toby Cox <toby.cox@netro42.com>
+     * @return stdClass|DOMDocument
+     */
+    public function request(string $endpoint)
+    {
+        $ch = curl_init($endpoint);
+        curl_setopt_array($ch, array(
+            CURLOPT_RETURNTRANSFER => 1,
+        ));
+
+        $response = curl_exec($ch);
+
+        return $response;
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# cakephp-instagramfeed-component
-CakePHP 2.x Component for fetching Instagram feed without OAuth
+# Instagram Feed Component For CakePHP 2.x
+
+CakePHP 2.x Component for fetching an Instagram feed without the need for OAuth or API access.
+
+## Installation
+
+- Add the InstagramfeedComponent.php file into your app here /app/Controllers/Components/
+- Make sure you are calling the component in either your AppController or the controller you are using:
+
+``` php
+public $components = array('RequestHandler', 'Instagramfeed');
+```
+
+or if there is no line "public $components", simply set it 
+
+``` php
+public $components = array('Instagramfeed');
+```
+
+Then in your controller, simply call it with your instagram username and how many images you would like returned
+
+## Example
+``` php
+$instagramfeed = $this->Instagramfeed->getFeed('netro42', 3);
+```
